7a09b539a8128f58bfe96a2208eeb021a81ca8c6
Shiferino <45627140+Shiferino@users.noreply.github.com>
2019-01-23 23:13:30 +0100

    Anpassungen

--- node-js-getting-started/index.js
+++ node-js-getting-started/index.js
@@ -1,12 +1,17 @@
-const express = require('express')
-const cool = require('cool-ascii-faces')
-const path = require('path')
-const PORT = process.env.PORT || 8080
+var express = require('express'); 
+var app = express();
+const PORT = process.env.PORT || 8080;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req,res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/', function (req, res) { // Routing to /
+res.send('Hallo! Dies ist der Heroku Test, sie haben ihn bestanden '); })
+
+app.get('/info', function (req, res) { // Routing to /info
+res.send('Hallo! Hier finden sie infos über die Gruppe 22'); })
+
+
+var server = app.listen(PORT, function () {
+var host = server.address().address
+var port = server.address().port
+console.log("Example app listening at http://%s:%s", host, port)
+}) // Achtung: Die Routings werden sequentiell geprüft und
+// bei Erfolg wird die Suche beendet !
